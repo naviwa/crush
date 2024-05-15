e2a6197bb4a0908efe8252540778f974b0c18310
naviwa <113837978+naviwa@users.noreply.github.com>
2024-05-15 14:52:33 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Kệ em, anh chỉ đùa thôi",
 };
